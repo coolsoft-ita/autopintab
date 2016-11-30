95c5c89cb86c2be059fa5506cc8354a317912fc6
nicorac <coolsoft.ita@gmail.com>
2016-11-30 16:29:58 +0100

    Updated build script, now adds manifest version to generated filename

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-rm -f autopintab.zip
+VER=$(sed -ne "s/ *\"version\": ['\"]\([^'\"]*\)['\"] *,/\1/p" manifest.json)
+FILENAME=autopintab-$VER.zip
 
-zip autopintab.zip \
+# delete existing file
+rm -f $FILENAME
+
+# create new
+zip $FILENAME \
   manifest.json \
   README.md     \
   background/*  \
   icons/*       \
   js/*          \
   options/*
+
+# completed
+echo
+echo "ZIP file" $FILENAME "created"
