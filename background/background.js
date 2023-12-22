7666d526cd2897265c6288ecfec5d03c3abf83ec
nicorac <coolsoft.ita@gmail.com>
2023-12-22 15:44:49 +0100

    Updated copyright messages

--- background/background.js
+++ background/background.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Claudio Nicora <coolsoft.ita@gmail.com>
+ * Copyright (C) 2024 Claudio Nicora <coolsoft.ita@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
